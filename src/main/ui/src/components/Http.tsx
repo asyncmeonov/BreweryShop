342c096617b6ec812faa980fbe0bcc3fa6a9245b
asyncmeonov <angel.st.sim@gmail.com>
2021-06-29 14:06:36 +0100

    Correct baseurl fe

--- src/main/ui/src/components/Http.tsx
+++ src/main/ui/src/components/Http.tsx
@@ -1,4 +1,4 @@
-const base = "http://localhost:8080";
+const base = "";
 
 async function get<T>(url: string): Promise<T> {
   let headers = new Headers({ "Content-Type": "application/json" });
@@ -12,7 +12,7 @@ async function post(url: string, content: any): Promise<Response> {
   let headers = new Headers({ "Content-Type": "application/json" });
   headers.append("Authorization", "Bearer " + window.token);
 
-  return await (await fetch(base + url, {
+  return (await fetch(base + url, {
     method: "POST",
     headers: headers,
     body: JSON.stringify(content)
@@ -23,7 +23,7 @@ async function remove(url: string, content: any): Promise<Response> {
   let headers = new Headers({ "Content-Type": "application/json" });
   headers.append("Authorization", "Bearer " + window.token);
 
-  return await (await fetch(base + url, {
+  return (await fetch(base + url, {
     method: "DELETE",
     headers: headers,
     body: JSON.stringify(content)

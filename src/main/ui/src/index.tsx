28aa691a834b169873d75868db9e0267c4d0624a
asyncmeonov <angel.st.sim@gmail.com>
2021-07-02 13:04:55 +0100

    Using HashRouter instead of Browser

--- src/main/ui/src/index.tsx
+++ src/main/ui/src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const client = new QueryClient();
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   <QueryClientProvider client={client}>
       <App />
   </QueryClientProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );

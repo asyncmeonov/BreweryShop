6345fd73637a09fca9878a981194769b57e68064
asyncmeonov <angel.st.sim@gmail.com>
2021-07-10 13:37:07 +0100

    Major OrderView overhaul + delivery date select + error validation + popup on delivery errors (untested) + reroute to beer view instead of login page on order submission

--- src/main/ui/src/components/Order/OrderSubmission.tsx
+++ src/main/ui/src/components/Order/OrderSubmission.tsx
@@ -1,4 +1,5 @@
-import { Container } from '@material-ui/core';
+import { Button, Container } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 
 type SubmissionProps = {
@@ -7,28 +8,31 @@ type SubmissionProps = {
 }
 
 const OrderSubmission = (props: SubmissionProps) => {
-  const {isSuccessful, message} = props;
+  const history = useHistory();
+  const { isSuccessful, message } = props;
   return (
-      <Container maxWidth="sm">
-        {
-          isSuccessful && (
-            <div>
-              <h1>Order was received!</h1>
-              <div>{message}</div>
-              <h3>Your order number is the confirmation for your order. Make sure you write it down</h3>
-            </div>
-          )
-        }
-        {
-          !isSuccessful && (
-            <div>
-              <h1>There was a problem with the order!</h1>
-              <div>{message}</div>
+    <Container maxWidth="sm">
+      {
+        isSuccessful && (
+          <div>
+            <h1>Order was received!</h1>
+            <div>{message}</div>
+            <h3>Your order number is the confirmation for your order. Make sure you write it down</h3>
           </div>
-          )
-        }
-        <div>Go back to the <a href="/">homepage</a></div>
-      </Container>
+        )
+      }
+      {
+        !isSuccessful && (
+          <div>
+            <h1>There was a problem with the order!</h1>
+            <div>{message}</div>
+          </div>
+        )
+      }
+      <Button onClick={() => history.push("/beers")}>
+        Go back to beer page
+      </Button>
+    </Container>
   );
 };
 
--- src/main/ui/src/components/Order/OrderView.tsx
+++ src/main/ui/src/components/Order/OrderView.tsx
@@ -1,104 +1,199 @@
 import { useState } from "react";
 //Components
-import { FormControl, FormHelperText, Input, InputLabel, Button } from "@material-ui/core";
+import { FormHelperText, Button, TextField, Box, Checkbox, FormControlLabel, Typography, LinearProgress } from "@material-ui/core";
 import { Container } from '@material-ui/core';
-import  OrderSubmission from "./OrderSubmission";
+import OrderSubmission from "./OrderSubmission";
 //styles
 import { Wrapper } from "./OrderView.style";
 import { BeerType, Order } from "../interfaces";
-import { post } from "../Http";
+import { get, post } from "../Http";
 import { useHistory } from "react-router-dom";
 import { getGlobalToken } from "../../window";
+import { Controller, useForm } from "react-hook-form";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useQuery } from "react-query";
+import { parseISO } from "date-fns";
+import { CustomSnackbarAlert } from "../../Alert";
 
 
-function isValidField(field: string | undefined): boolean {
-  return field !== undefined && field.length > 0
+const getDeliveryDates = async () => await get<string[]>("/delivery");
+
+const postOrder = async (content: Order, param: string) => await post("/order" + param, content);
+
+const parseDates = (rawDates: string[]): Date[] => rawDates.map(raw => parseDate(raw));
+
+const parseDate = (rawDate: string): Date => {
+  return parseISO(rawDate);
 }
-
-
-function isInvalidField(field: string | undefined): boolean {
-  return !isValidField(field)
-}
-
 const OrderView = () => {
   let history = useHistory();
+  const { control, handleSubmit, formState: { errors } } = useForm<Order>();
+  const { data, isLoading, error } = useQuery<string[]>(
+    "Date[]",
+    getDeliveryDates
+  );
   const [message, setMessage] = useState<string | undefined>(undefined);
   const [isSuccessful, setIsSuccessful] = useState(false);
-  const [pName, setPirateName] = useState<string | undefined>(undefined);
-  const [pContact, setPirateContact] = useState<string | undefined>(undefined);
-  const orderedBeers = (history.location.state as {order: BeerType[]}).order
+  const [isDelivery, setIsDelivery] = useState(true);
+  const [deliveryDate, setDeliveryDate] = useState<Date | undefined>();
+  const [errorPopupOpen, setErrorPopupOpen] = useState(false);
 
+  const orderedBeers = (history.location.state as { order: BeerType[] }).order
 
-  const postOrder = async(content: Order) => {
-    let response = await post("/order", content)
-    let responseBody = await response.text()
-    if(response.ok){
+  const handlePopupClose = () => {
+    setErrorPopupOpen(false);
+  }
+
+  const onSubmit = handleSubmit(async (data) => {
+    if(isInvalidDeliveryDate()) return
+    let param = (!isInvalidDeliveryDate() && isDelivery) ? "?delivery=" + deliveryDate?.toLocaleDateString() : "";
+    let response = await postOrder(data, param);
+    let responseBody = await response.text();
+    if (response.ok) {
       setMessage(responseBody)
       setIsSuccessful(true);
     } else {
       setMessage(`${responseBody} Please try again. If the problem persists, contact us.`)
       setIsSuccessful(false);
     }
-  }
+  })
+
+  const isInvalidDeliveryDate = () => isDelivery && (deliveryDate === undefined || deliveryDate === null)
 
   if (getGlobalToken() === undefined) {
     history.push({ pathname: "/", state: { hasExpired: true } })
   }
 
-  if(message) {
+  if (message) {
     return (
       <Wrapper>
-        <OrderSubmission {...{isSuccessful, message}}></OrderSubmission>
+        <OrderSubmission {...{ isSuccessful, message }}></OrderSubmission>
       </Wrapper>
     )
   }
 
-  return (
+  const formOptions = {
+    name: { required: "Name is required" },
+    contact: { required: "Contact is required" }
+  };
+
+  if (isLoading) {
+    return <LinearProgress />;
+  } else return (
     <Wrapper>
       <Container maxWidth="sm">
-        <hr/>
-        {
-          orderedBeers.map((beer, i) => (
-            <div key={`order-beer-${i}`}>
-              x{beer.amount} of {beer.name} {beer.size}ml ({(beer.price / 100).toFixed(2)} bgn each)
-            </div>
-          ))
-        }
-        <hr/>
-        {
-          <div>Total: {(orderedBeers.reduce((sum, current) => sum + (current.price * current.amount), 0)/100).toFixed(2)}</div>
-        }
-        <FormControl>
-            <InputLabel htmlFor="pirate-name" required={true}>Pirate Name</InputLabel>
-            <Input id="pirate-name" aria-describedby="pirate-name-helper-text"
-            onChange={e => setPirateName(e.target.value)}
-            inputProps={{ maxLength: 69 }}/>
-            <FormHelperText id="pirate-name-helper-text">How are you known in the seas? max 69 characters</FormHelperText>
-      </FormControl> <br/>
-      <FormControl>
-            <InputLabel htmlFor="pirate-contact" required={true}>Pirate Contact</InputLabel>
-            <Input 
-            id="pirate-contact" 
-            aria-describedby="pirate-contact-helper-text" 
-            multiline
-            onChange={e => setPirateContact(e.target.value)}
-            inputProps={{ maxLength: 420 }}/>
-            <FormHelperText id="pirate-contact-helper-text_1">How do you want to be contacted for the delivery? max 420 characters</FormHelperText>
-            <FormHelperText id="pirate-contact-helper-text_2">Phone, email or another unique way of contacting is needed to do the delivery.</FormHelperText>
-            <FormHelperText id="pirate-contact-helper-text_2"><i>Example: I will collect it myself next Thursday (11/06), email: bad.boy@gmail.com</i></FormHelperText>
-      </FormControl><br/>
-      <Button
+        <Box display="flex" flexDirection="column">
+          <Box>
+            <hr />
+            {
+              orderedBeers.map((beer, i) => (
+                <Controller
+                  key={`order-beer-${i}`}
+                  name={`orderBeers.${i}`}
+                  control={control}
+                  defaultValue={orderedBeers[i]}
+                  rules={formOptions.name}
+                  render={({ field }) => (
+                    <Box>
+                      x{beer.amount} of {beer.name} {beer.size}ml ({(beer.price / 100).toFixed(2)} bgn each)
+                    </Box>
+                  )}
+                />
+              ))
+            }
+            <hr />
+          </Box>
+
+          <Box alignSelf="right">Total: {(orderedBeers.reduce((sum, current) => sum + (current.price * current.amount), 0) / 100).toFixed(2)}</Box>
+
+        </Box>
+        <Controller
+          name="pirateName"
+          control={control}
+          defaultValue=""
+          rules={formOptions.name}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              autoFocus
+              margin="dense"
+              label="Your Name"
+              fullWidth
+              inputProps={{ maxLength: 50 }}
+              InputLabelProps={{ required: true }}
+              error={errors.pirateName !== undefined}
+              helperText={errors.pirateName && errors.pirateName.message}
+            />
+          )}
+        />
+        <Controller
+          name="pirateContact"
+          control={control}
+          defaultValue=""
+          rules={formOptions.contact}
+          render={({ field }) => (
+            <Box>
+              <TextField
+                {...field}
+                margin="dense"
+                label="Contact"
+                fullWidth
+                multiline
+                inputProps={{ maxLength: 300 }}
+                InputLabelProps={{ required: true }}
+                error={errors.pirateContact !== undefined}
+                helperText={errors.pirateContact && errors.pirateContact.message}
+              />
+              <FormHelperText>How do you want to be contacted for the delivery?</FormHelperText>
+              <FormHelperText>Phone, email or another unique way of contacting is needed to do the delivery.</FormHelperText>
+              <FormHelperText><b>Note:</b> we wouldn't be able to deliver if we don't have a meaningful contact</FormHelperText>
+            </Box>
+          )}
+        />
+        <Box display="flex" alignItems="center" justifyContent="space-between" marginBottom="2em">
+          <FormControlLabel
+            label="I want a delivery."
+            control={
+              <Checkbox
+                disabled={error !== null}
+                checked={isDelivery && error === null}
+                onChange={() => setIsDelivery(!isDelivery)}
+                color="primary"
+              />}
+          />
+          <Box>
+            <Typography variant="subtitle2" color={(isInvalidDeliveryDate())? "error": "textSecondary"}>Delivery Date *</Typography>
+            {data &&
+              <DatePicker
+                disabled={!isDelivery}
+                required={isDelivery}
+                selected={deliveryDate}
+                onChange={(date) => setDeliveryDate(date as Date | undefined)}
+                includeDates={parseDates(data)}
+                dateFormat="dd/MM/yyyy"
+                placeholderText="dd/MM/yyyy"
+              />
+            }
+          </Box>
+        </Box>
+        <Button
           size="large"
           disableElevation
           variant="contained"
-          disabled={(isInvalidField(pName) || isInvalidField(pContact))}
-          onClick={() => postOrder({orderBeers: orderedBeers, pirateName: pName as string, pirateContact: pContact as string })}
+          onClick={(e) => onSubmit(e)}
         >
           Submit Order
         </Button>
       </Container>
-    </Wrapper>
-  );
+      {error &&
+        <CustomSnackbarAlert {...{
+          open: errorPopupOpen,
+          onClose: handlePopupClose,
+          severity: "error",
+          contentText: "Currently there is a problem with selecting deliveries. Please refresh the page and if the problem persists, contact us."
+        }} />}
+    </Wrapper>);
 };
 
 export default OrderView;
